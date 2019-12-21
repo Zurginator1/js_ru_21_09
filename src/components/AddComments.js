d176494e28b2fda48e8cb95d5d135e30af020ade
Legolas <razatr@yandex.ru>
2019-12-21 18:25:34 +0300

    edit struct

--- src/components/AddComments.js
+++ src/components/AddComments.js
@@ -2,38 +2,61 @@ import React, { Component } from 'react'
 
 class AddComment extends Component {
     state = {
-        username: '',
-        text: '',
-        usernameStyle: {color : 'red'},
-        textStyle: {color : 'red'}
+        values: {
+            username: '',
+            text: ''
+        },
+        errors: {}
     }
 
-    handleSubmit = (event) => {
+    componentDidMount() {
+        const { values: { username, text } } = this.state;
+
+        this.setState({
+            errors: {
+                username: this.validate(username) ? true : undefined,
+                text: this.validate(text) ? true : undefined
+            }
+        });
+    }
+
+    validate(value) {
+        return value.length < 10 || value.length > 50;
+    }
+
+    formIsValid() {
+        const { errors } = this.state;
+        for (let key in errors) {
+            if (errors[key])
+                return false;
+        }
+        return true;
+    }
+
+    handleSubmit = event => {
         event.preventDefault();
-        if(this.state.usernameStyle.color === 'red' || this.state.textStyle.color === 'red')
-            return
-        this.setState({ username: '', text: '' })
+        if (this.formIsValid())
+            return;
+        this.setState({ values: { username: '', text: '' } })
     }
 
     handleChange = ev => {
         const { target: { value, name } } = ev;
-        this.setState({ [name]: value });
-        const style = name + 'Style';
-
-        if (value.length < 10 || value.length > 50) {
-            this.setState({ [style]: { color: 'red' } })
-        }
-        else
-            this.setState({ [style]: { color: 'black' } });
+        this.setState({
+            values: { [name]: value },
+            errors: {
+                [name]: this.validate(value) ? true : undefined
+            }
+        });
     }
 
     render() {
-        const { username, text, usernameStyle, textStyle } = this.state;
+        const { values: { username, text }, errors: { username: usernameError, text: textError } } = this.state;
 
         return (
             <form onSubmit={this.handleSubmit} >
-                <b>Username</b>: <input type="text" style={usernameStyle} name="username" value={username} onChange={this.handleChange} />
-                <b>Comment</b>: <input type="text" style={textStyle} name="text" value={text} onChange={this.handleChange} />
+                <b>Username</b>: <input type="text" style={{ color: usernameError ? 'red' : 'black' }} name="username" value={username} onChange={this.handleChange} />
+                <b>Comment</b>: <input type="text" style={{ color: textError ? 'red' : 'black' }} name="text" value={text} onChange={this.handleChange} />
                 <input type="submit" value="Let's go comment" />
             </form>
         )
